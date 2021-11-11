d7fef0b7a0998b9a8454d407b54266af40c7db45
Jaqueline Mejia <j.mejia@asimov.mx>
2021-11-11 00:50:29 -0600

    tarea 02 hecha

--- robot_comm/src/t02.py
+++ robot_comm/src/t02.py
@@ -1,177 +1,131 @@
 #!/usr/bin/env python3
 
+from numpy.lib.arraysetops import isin
 import rospy
-from geometry_msgs.msg import Twist,Point
-import sys, select, os
 from nav_msgs.msg import Odometry
-from pubsub.msg import Status
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Point, Twist
+from math import atan2
+import numpy
+from rospy.names import valid_name_validator_resolved
+from sensor_msgs.msg import LaserScan
+import time
 
-from re import sub
-from threading import current_thread
-from nav_msgs import msg
-import rospy
-import math
+# Generamos una funcion para obtener nuestra posicion en X y Y
+def newOdom(msg):
 
-WAFFLE_MAX_LIN_VEL = 1
-WAFFLE_MAX_ANG_VEL = 2
+    # Generamos variables globales para usarlas en cualquier lado
+    global x
+    global y
+    global theta
 
-LIN_VEL_STEP_SIZE = 0.5
-ANG_VEL_STEP_SIZE = 0.5
+    # Obtenemos la posicion en X y Y y las guardamos en las variables x,y
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    # Obtenemos la orientacioin del robot
+    rot_q = msg.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
-msg = """
-Controla tu robot!
----------------------------
-Comandos de movimiento:
 
-Escribe la coordenada en X a la que quieres llegar
+# Generamos una funcion para poder hacer funcionar el sensor
+def sensor_callback(msg):
+    global wall
+    wall = msg.ranges[300]
 
-Escribe la coordenada en Y a la que quieres llegar
+# Iniciamos el noto
+rospy.init_node("speed_controller")
 
-Escribe "Exit" o "exit" para salir del programa
-"""
+# Nos suscribimos y publicamos
+sub = rospy.Subscriber("/odom", Odometry, newOdom)
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+sub_2 = rospy.Subscriber('/scan', LaserScan, sensor_callback)
 
-e = """Fallo la comunicacion"""
+# Ponemos todo en orden para inicializar las variables
+speed = Twist()
 
-def makeSimpleProfile(output, input, slop):
-    if input > output:
-        output = min( input, output + slop)
-    elif input < output:
-        output = max( input, output - slop)
+r = rospy.Rate(4)
+x = 0.0
+y = 0.0
+theta = 0.0
+wall = 0.0
+count = 0
+
+# Asignamos la funcion Point() a una variable para tener comodidad al escribir
+goal = Point()
+
+# Pedimos por primera vez nuestros datos
+goal.x = float(input('Escribe la coordenada en x --> '))
+goal.y = float(input('Escribe la coordenada en y --> '))
+# Preguntamos si queremos visualizar la posicion en la que se encuentra el robot
+option = int(input("Deseas saber tus coordenadas? [S = 1][N = 0] --> "))
+
+
+# Iniciamos el programa principal
+while not rospy.is_shutdown():
+
+    # Creamos la condicion donde imprimimos las coordenadas o no
+    if option == 1:
+        print('Estamos en la coordenada (',round(x,2),' , ',round(y,2),')')
+
+    # Calculamos la distancia que existe entre las coordenadas actuales y las que asignamos
+    inc_x = goal.x -x
+    inc_y = goal.y -y
+
+    #Calculamos el angulo que existe entre los puntos   
+    angle_to_goal = atan2(inc_y, inc_x)
+
+    #print(wall)
+    #print(angle_to_goal)
+    #print(round(inc_x,2),round(inc_y,2))
+    if abs(angle_to_goal - theta) > 1:
+        speed.linear.x = 0.0
+        speed.angular.z = 0.3
+    elif wall <= 1:
+        print("Obstaculo")
+        speed.linear.x = -0.3
+        speed.angular.z = 0.0
+        pub.publish(speed)
+        time.sleep(2)
+        speed.linear.x = 0.0
+        speed.angular.z = 0.3
+        pub.publish(speed)
+        time.sleep(5)
+        speed.linear.x = 0.5
+        speed.angular.z = 0.0
+        pub.publish(speed)
+        time.sleep(6)
     else:
-        output = input
+        speed.linear.x = 0.2
+        speed.angular.z = 0.0
 
-    return output
+    # Creamos dos rangos que nos serviran como tolerancia para que el robot no se quede girando
+    rango_x = numpy.arange(-0.05,0.05,0.01)
+    rango_y = numpy.arange(-0.05,0.05,0.01)
 
-def constrain(input, low, high):
-    if input < low:
-      input = low
-    elif input > high:
-      input = high
-    else:
-      input = input
+    # Creamos la condicion donde preguntaremos si inc_x e inc_y estan dentro de nuestra tolerancia
+    if round(inc_x,2) in rango_x and round(inc_y,2) in rango_y:
+        # En caso de que si estemos en la tolerancia paramos el robot
+        speed.linear.x = 0.0
+        speed.angular.z = 0.0
+        # Publicamos la velocidad a nuestro robot
+        pub.publish(speed)
+        r.sleep()
+        print('Hemos llegado a las coordenadas [x = ',goal.x,'y = ',goal.y,']')
+        
+        # Creamos una variable para preguntar si queremos coordenadas nuevas
+        aux = input('Quieres coordenadas nuevas? [S = 1][N = 0] --> ')
+        if aux == 0 or aux == 0: # En caso de que no, entonces el programa terminara
+            exit()
+        elif aux == 1 or aux == 1: # En caso de que si, entonces pedimos los mismos datos de nuevo
+            print("Va va va va ya estas, entonces vamos de nuevo 7u7r")
+            goal.x = int(input('Escribe la coordenada en x --> '))
+            goal.y = int(input('Escribe la coordenada en y --> '))
+            option = int(input("Deseas saber tus coordenadas? [S = 1][N = 0] --> "))
+        else:
+            print('Ingresa bien lo que se pide por favor UnU, no seas malito :(')
 
-    return input
+ 
+    # Publicamos a nuestro robot la velocidad calculada
 
-def checkLinearLimitVelocity(vel):
-    if turtlebot3_model == "waffle" or turtlebot3_model == "waffle_pi":
-      vel = constrain(vel, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
-    return vel
-
-def checkAngularLimitVelocity(vel):
-    if turtlebot3_model == "waffle" or turtlebot3_model == "waffle_pi":
-      vel = constrain(vel, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
-    return vel
-
-def vel_new(control_linear_vel,control_angular_vel):
-    twist = Twist()
-    control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
-    twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
-    control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
-    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
-    pub.publish(twist)
-
-
-# .................. Nuevas funciones ..................
-
-def inicio(): # Creamos una funcion para inicializar todo
-    odom_msg = None
-    current_position = None
-    while odom_msg is None:
-        try:
-            odom_msg = rospy.wait_for_message('/odom',Odometry,timeout=10)
-        except Exception as e:
-            rospy.logerr(str(e))
-            #exit()
-
-    current_position = odom_msg.pose.pose.position #Guardamos el valor de la posicion
-    
-    return current_position
-
-def callback_llamada(msg):
-    current_position = msg.pose.pose.position
-
-def extrae_plano(position):
-    return (position.x, position.y)
-
-if __name__=="__main__":
-    rospy.init_node('turtlebot3_teleop')
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    #rospy.init_node('monitor')
-    sub = rospy.Subscriber('/odom',Odometry,callback_llamada)
-
-    # Obtenemos el modelo de turtlebot (waffle)
-    turtlebot3_model = rospy.get_param("model", "waffle")
-
-    # Inicializamos nuestras variables para no tener errores
-    status = 0
-    target_linear_vel   = 0.0
-    target_angular_vel  = 0.0
-    control_linear_vel  = 0.0
-    control_angular_vel = 0.0
-
-
-    #Comenzamos a pedir nuetras variables
-    try:
-        print(msg)
-        print("\n\nIngresa las coordenadas (X,Y) que deseas\n\n")
-        while(1):
-            # Pedimos las coordenadas en X y Y
-            keyx=int(input("Coordenada en X --> "))
-            keyy=int(input("Coordenada en Y --> "))
-
-            #Obtnemos las coordenadas en las que nos encontramos
-            pos = extrae_plano(inicio())
-            posx=round(pos[0],4)
-            posy=round(pos[1],4)
-
-            # Preguntamos la accion y dependiendo de eso hacemos lo que nos indica
-            while (keyx != posx):
-
-                # La variable posx es la que nos dice donde nos encontramos respecto a x
-                if posx < keyx: # Si la coordenada en x es negativa vamos hacia delante
-                    target_linear_vel = checkLinearLimitVelocity(target_linear_vel + LIN_VEL_STEP_SIZE)
-                    status = status + 1
-                elif posx > keyx: # Si la coordenada en x es positiva vamos hacia atras
-                    target_linear_vel = checkLinearLimitVelocity(target_linear_vel - LIN_VEL_STEP_SIZE)
-                    status = status + 1
-
-                if keyx == posx: # Si hemos llegado a la posicion que queremos, nos detenemos
-                    print("Hemos llegado a la coordenada en x")
-                    target_linear_vel   = 0.0
-                    control_linear_vel  = 0.0
-                    target_angular_vel  = 0.0
-                    control_angular_vel = 0.0
-
-                # Iniciamos la parte donde reasignamos las nuevas velocidades                
-                vel_new()
-
-            while (keyy != posy):
-                # La variable posy es la que nos dice donde nos encontramos respecto a y
-                # La variable posx la podemos encontrar en uno de los programas hechos por el profesor
-                
-                if posy < keyy: # Si la coordenada en y es negativa vamos hacia la izquierda
-                    target_angular_vel = checkAngularLimitVelocity(target_angular_vel + ANG_VEL_STEP_SIZE)
-                    status = status + 1
-
-                elif posy > keyy: # Si la coordenada en y es positiva vamos hacia la derecha
-                    target_angular_vel = checkAngularLimitVelocity(target_angular_vel - ANG_VEL_STEP_SIZE)
-                    status = status + 1
-
-                if keyy == posy: # Si hemos llegado a la posicion que queremos, nos detenemos
-                    print("Hemos llegado a la coordenada en y")
-                    target_linear_vel   = 0.0
-                    control_linear_vel  = 0.0
-                    target_angular_vel  = 0.0
-                    control_angular_vel = 0.0
-                
-                # Iniciamos la parte donde reasignamos las nuevas velocidades                
-                vel_new()
-
-    except:
-        print(e)
-
-    finally:
-        twist = Twist()
-        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
-        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-        pub.publish(twist)
+    pub.publish(speed)
+    r.sleep()
